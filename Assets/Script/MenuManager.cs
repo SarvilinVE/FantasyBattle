16767b1ce1a137568cf03514684438f645d1c654
SarvilinVE <73125277+SarvilinVE@users.noreply.github.com>
2022-08-07 18:51:53 +0500

    Добавлен UI для лобби

--- Assets/Script/MenuManager.cs
+++ Assets/Script/MenuManager.cs
@@ -1,3 +1,4 @@
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -17,38 +18,65 @@ public class MenuManager : MonoBehaviour
     private TMP_Text _catalogText;
 
     [SerializeField]
+    private Button _lobby;
+
+    [SerializeField]
+    private TMP_Text _lobbyText;
+
+    [SerializeField]
     private GameObject _userInfoPanel;
 
     [SerializeField]
     private GameObject _catalogPanel;
 
+    [SerializeField]
+    private GameObject _lobbyPanel;
+
     void Start()
     {
         _userInfoPanel.SetActive(true);
         _catalogPanel.SetActive(false);
+        _lobbyPanel.SetActive(false);
 
         _userInfoText.color = Color.green;
         _catalogText.color = Color.black;
+        _lobbyText.color = Color.black;
 
         _userInfo.onClick.AddListener(ShowInfo);
         _catalog.onClick.AddListener(ShowCatalog);
+        _lobby.onClick.AddListener(ShowLobby);
+    }
+
+    private void ShowLobby()
+    {
+        _userInfoPanel.SetActive(false);
+        _catalogPanel.SetActive(false);
+        _lobbyPanel.SetActive(true);
+
+        _userInfoText.color = Color.black;
+        _catalogText.color = Color.black;
+        _lobbyText.color = Color.green;
     }
 
     private void ShowCatalog()
     {
         _userInfoPanel.SetActive(false);
         _catalogPanel.SetActive(true);
+        _lobbyPanel.SetActive(false);
 
         _userInfoText.color = Color.black;
         _catalogText.color = Color.green;
+        _lobbyText.color = Color.black;
     }
 
     private void ShowInfo()
     {
         _userInfoPanel.SetActive(true);
         _catalogPanel.SetActive(false);
+        _lobbyPanel.SetActive(false);
 
         _userInfoText.color = Color.green;
         _catalogText.color = Color.black;
+        _lobbyText.color = Color.black;
     }
 }
---
+++ Assets/Script/Photon/ConnectAndJoinRandomLobby.cs
@@ -0,0 +1,195 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Realtime;
+using Photon.Pun;
+using TMPro;
+
+public class ConnectAndJoinRandomLobby : MonoBehaviour, IConnectionCallbacks, IMatchmakingCallbacks, ILobbyCallbacks
+{
+    [SerializeField]
+    private ServerSettings _serverSettings;
+
+    [SerializeField]
+    private TMP_Text _stateUiText;
+
+    private LoadBalancingClient _lbc;
+
+    private const string AI_KEY = "ai";
+    private const string PLAYERS_KEY = "rp";
+
+    private const string EXP_KEY = "C0";
+    private const string MAP_KEY = "C1";
+
+    private TypedLobby _sqlLobby = new TypedLobby("sqlLobby", LobbyType.SqlLobby);
+
+    private TypedLobby _customLobby = new TypedLobby("customLobby", LobbyType.Default);
+
+    private Dictionary<string, RoomInfo> _cachedRoomList = new Dictionary<string, RoomInfo>();
+
+
+    private void Start()
+    {
+        _lbc = new LoadBalancingClient();
+        _lbc.AddCallbackTarget(this);
+
+        _lbc.ConnectUsingSettings(_serverSettings.AppSettings);
+    }
+    private void Update()
+    {
+        if (_lbc == null)
+            return;
+
+        _lbc.Service();
+
+        var state = _lbc.State.ToString();
+        if(state == "Joined")
+        _stateUiText.text = $"State: {state}, UserID: {_lbc.UserId}, Room: {_lbc.CurrentRoom.Name}";
+    }
+    private void OnDestroy()
+    {
+        _lbc.RemoveCallbackTarget(this);
+    }
+    public void OnConnected()
+    {
+
+    }
+
+    public void OnConnectedToMaster()
+    {
+        Debug.Log("On connected master");
+
+        var roomOptions = new RoomOptions
+        {
+            MaxPlayers = 4,
+            IsVisible = true,
+            CustomRoomPropertiesForLobby = new[]
+            {
+                EXP_KEY,
+                MAP_KEY
+            },
+            CustomRoomProperties = new ExitGames.Client.Photon.Hashtable
+            {
+                {EXP_KEY, 400 },
+                {MAP_KEY, "Green Garden" }
+            }
+        };
+
+        var enterRoomParams = new EnterRoomParams
+        {
+            RoomName = "NewRoom",
+            RoomOptions = roomOptions,
+            ExpectedUsers = new[] {"@sd78s76awwa"},
+            Lobby = _customLobby
+        };
+
+        //_lbc.OpCreateRoom(enterRoomParams);
+        _lbc.OpJoinLobby(_customLobby);
+    }
+
+    private void UpdateCachedRoomList(List<RoomInfo> roomList)
+    {
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            RoomInfo info = roomList[i];
+            if (info.RemovedFromList)
+            {
+                _cachedRoomList.Remove(info.Name);
+            }
+            else
+            {
+                _cachedRoomList[info.Name] = info;
+            }
+        }
+    }
+
+
+    public void OnCreatedRoom()
+    {
+
+    }
+
+    public void OnCreateRoomFailed(short returnCode, string message)
+    {
+        
+    }
+
+    public void OnCustomAuthenticationFailed(string debugMessage)
+    {
+        
+    }
+
+    public void OnCustomAuthenticationResponse(Dictionary<string, object> data)
+    {
+
+    }
+
+    public void OnDisconnected(DisconnectCause cause)
+    {
+
+    }
+
+    public void OnFriendListUpdate(List<FriendInfo> friendList)
+    {
+
+    }
+
+    public void OnJoinedLobby()
+    {
+        //_cachedRoomList.Clear();
+
+        //var sqlLobbyFilter = $"{EXP_KEY} BETWEEN 300 AND 500 AND {MAP_KEY} = 'Green Garden'";
+        //var opJoinRooomParams = new OpJoinRandomRoomParams
+        //{
+        //    SqlLobbyFilter = sqlLobbyFilter
+        //};
+
+        //Debug.Log("On join Lobby");
+        _lbc.OpCreateRoom(new EnterRoomParams());
+
+        Debug.Log("On join Lobby");
+    }
+
+    public void OnJoinedRoom()
+    {
+        Debug.Log("On join room");
+     }
+
+    public void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("On join room failsed");
+        
+    }
+
+    public void OnJoinRoomFailed(short returnCode, string message)
+    {
+
+    }
+
+    public void OnLeftLobby()
+    {
+
+    }
+
+    public void OnLeftRoom()
+    {
+        Debug.Log("On left room");
+    }
+
+    public void OnLobbyStatisticsUpdate(List<TypedLobbyInfo> lobbyStatistics)
+    {
+
+    }
+
+    public void OnRegionListReceived(RegionHandler regionHandler)
+    {
+
+    }
+
+    public void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        UpdateCachedRoomList(roomList);
+
+        Debug.Log($"Count room: {roomList.Count}");
+    }
+}
